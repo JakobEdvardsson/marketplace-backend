d5f1ee0e75f23de1689ae2d615cc93d27e21d8a3
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-04-17 14:11:19 +0200

    Order class is ready
    trust me

--- src/main/java/org/example/marketplacebackend/model/Order.java
+++ src/main/java/org/example/marketplacebackend/model/Order.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,6 +24,9 @@ public class Order {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private UUID order;
+
+  @ManyToOne
+  private Account account;
 
   private Instant timeOfPurchase;
 
