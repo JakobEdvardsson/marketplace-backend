b19f9a034df8111b3a4e1025a9788220072d63be
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-04-17 15:04:43 +0200

    Added Inbox and WatchList classes

--- src/main/java/org/example/marketplacebackend/model/Inbox.java
+++ src/main/java/org/example/marketplacebackend/model/Inbox.java
@@ -1,5 +1,32 @@
 package org.example.marketplacebackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.UUID;
+
+@Getter @Setter @NoArgsConstructor @EqualsAndHashCode @ToString
+@Entity
+@Table
 public class Inbox {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  @OneToOne
+  private Account account;
+
+  private String message;
+
+  private boolean isRead;
 
 }
--- src/main/java/org/example/marketplacebackend/model/WatchList.java
+++ src/main/java/org/example/marketplacebackend/model/WatchList.java
@@ -1,5 +1,33 @@
 package org.example.marketplacebackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.UUID;
+
+@Getter @Setter @NoArgsConstructor @EqualsAndHashCode @ToString
+@Entity
+@Table
 public class WatchList {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  @OneToOne
+  private Account account;
+
+  @ManyToMany
+  private ProductType productTypeId;
+  
 }
