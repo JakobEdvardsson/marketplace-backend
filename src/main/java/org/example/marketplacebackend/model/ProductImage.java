a026b4a5d1068a582fc58f60c76ab63c4cc8e7f0
Filip Bengtegård <bengtegardbook@gmail.com>
2024-04-17 09:09:27 +0200

    add ProductImage model class and repository class

--- src/main/java/org/example/marketplacebackend/model/ProductImage.java
+++ src/main/java/org/example/marketplacebackend/model/ProductImage.java
@@ -1,5 +1,44 @@
 package org.example.marketplacebackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "product_image")
 public class ProductImage {
 
+  @Id
+  private Long id;
+  private String imageUrl;
+
+  public ProductImage(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o)
+      return true;
+    if (!(o instanceof ProductImage image))
+      return false;
+
+    return Objects.equals(this.id, image.id) && Objects.equals(this.imageUrl, image.imageUrl);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id, this.imageUrl);
+  }
+
+  @Override
+  public String toString() {
+    return "ProductImage{" + "id=" + this.id + ", url='" + this.imageUrl + '}';
+  }
 }
