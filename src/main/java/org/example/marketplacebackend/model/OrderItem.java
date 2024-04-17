7f64a10b561ac762ac84a47986111565544cdd3e
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-04-17 14:04:21 +0200

    OrderItem class is done
    (i believe)

--- src/main/java/org/example/marketplacebackend/model/OrderItem.java
+++ src/main/java/org/example/marketplacebackend/model/OrderItem.java
@@ -1,28 +1,32 @@
 package org.example.marketplacebackend.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import java.time.Instant;
 import java.util.UUID;
 
 @Getter @Setter @NoArgsConstructor @EqualsAndHashCode @ToString
 @Entity
 @Table
-public class Order {
+public class OrderItem {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private UUID order;
+  private UUID id;
 
-  private Instant timeOfPurchase;
+  @OneToOne
+  private Order order;
+
+  @OneToOne
+  private Product product;
 
 }
