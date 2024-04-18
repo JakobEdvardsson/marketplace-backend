65fce13bcda5030ce385cb6b6b70d4a8e6e80444
Oliver Berggren <oliverberggren00@gmail.com>
2024-04-18 14:26:22 +0200

    Added to AccountController

--- src/main/java/org/example/marketplacebackend/controller/AccountsController.java
+++ src/main/java/org/example/marketplacebackend/controller/AccountsController.java
@@ -1,24 +1,33 @@
 package org.example.marketplacebackend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.marketplacebackend.DTO.incoming.UserDTO;
+import org.example.marketplacebackend.model.Account;
 import org.example.marketplacebackend.repository.AccountRepository;
+import org.example.marketplacebackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Map;
 
-@RequestMapping("v1/account")
+@RequestMapping("v1/accounts")
 @CrossOrigin(origins = "localhost:3000", allowCredentials = "true")
-@Controller
+@RestController
 public class AccountsController {
-  private final AccountRepository accountRepository;
+  private final UserService userService;
 
-  public AccountsController (AccountRepository accountRepository) {
-    this.accountRepository = accountRepository;
+  public AccountsController(UserService userService) {
+    this.userService = userService;
+
   }
 
-  @GetMapping("/test")
-  public Map<String, Object> greeting() {
-    return Collections.singletonMap("message", "Hello, World");
+  @PostMapping ( "/register")
+  public ResponseEntity<String> register(@RequestBody Account user, HttpServletRequest httpServletRequest) {
+    userService.saveUser(user);
+    return ResponseEntity.ok().body("Success");
   }
 
 }
