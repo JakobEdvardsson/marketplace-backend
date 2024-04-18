62b58f4891c35e4485ddc1a0e7e199dae91378b1
Filip Bengtegård <bengtegardbook@gmail.com>
2024-04-18 14:11:28 +0200

    add UserDTO file

---
+++ src/main/java/org/example/marketplacebackend/DTO/incoming/UserDTO.java
@@ -0,0 +1,16 @@
+package org.example.marketplacebackend.DTO.incoming;
+
+import java.sql.Date;
+
+/**
+ * DTO used for sending user registration form data.
+ * @param firstName
+ * @param lastName
+ * @param email
+ * @param username
+ * @param password
+ * @param date_of_birth
+ */
+public record UserDTO (String firstName, String lastName, String email, String username, String password, Date date_of_birth){
+
+}
