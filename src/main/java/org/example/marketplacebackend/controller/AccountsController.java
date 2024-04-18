b45bc50dd2497da03841c5188c3a77aec005ccd4
Oliver Berggren <oliverberggren00@gmail.com>
2024-04-18 13:46:24 +0200

    beginning of account-controller

--- src/main/java/org/example/marketplacebackend/controller/AccountsController.java
+++ src/main/java/org/example/marketplacebackend/controller/AccountsController.java
@@ -1,5 +1,24 @@
 package org.example.marketplacebackend.controller;
 
+import org.example.marketplacebackend.repository.AccountRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.Collections;
+import java.util.Map;
+
+@RequestMapping("v1/account")
+@CrossOrigin(origins = "localhost:3000", allowCredentials = "true")
+@Controller
 public class AccountsController {
+  private final AccountRepository accountRepository;
+
+  public AccountsController (AccountRepository accountRepository) {
+    this.accountRepository = accountRepository;
+  }
+
+  @GetMapping("/test")
+  public Map<String, Object> greeting() {
+    return Collections.singletonMap("message", "Hello, World");
+  }
 
 }
