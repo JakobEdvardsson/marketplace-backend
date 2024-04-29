09dd91f4a71e6b138523bb0f3d55929474b5b61a
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-04-30 00:30:49 +0200

    Add db setup for TestProductsEndpoints tests
    Yes, this looks terrible.

--- src/test/java/org/example/marketplacebackend/TestProductEndpoints.java
+++ src/test/java/org/example/marketplacebackend/TestProductEndpoints.java
@@ -18,6 +18,8 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,7 +27,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.UUID;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -75,9 +76,19 @@ public class TestProductEndpoints {
   }
 
   @Test
-  void uploadProductSuccess() throws Exception {
-    UUID productType = UUID.fromString("d5509745-450f-4760-8bdd-ddc88d376b37");
-    ProductDTO product = new ProductDTO("test", productType,
+  @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
+      statements = """
+      INSERT INTO account (id, first_name, last_name, date_of_birth, email, password, username) VALUES ('3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', 'Ken', 'Thompson', '1943-02-04', 'ken@example.com', '$2a$10$gIwb60Eio1J1UYWqCrV4je9kAzsqra0kzwg5fcKRCauzGUQ2xmx3q', 'ken');
+      INSERT INTO product_category VALUES ('d5509745-450f-4760-8bdd-ddc88d376b37', 'electronics');
+      """)
+  @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
+      statements = """
+      DELETE FROM account WHERE id = '3a45dc5e-2a30-41ba-b488-ca4b113ea5ee';
+      DELETE FROM product_category WHERE id = 'd5509745-450f-4760-8bdd-ddc88d376b37';
+      """)
+  public void uploadProductSuccess() throws Exception {
+    UUID productCategory = UUID.fromString("d5509745-450f-4760-8bdd-ddc88d376b37");
+    ProductDTO product = new ProductDTO("test", productCategory,
         500, 0, "wow amazing",
         null, null);
 
@@ -97,7 +108,7 @@ public class TestProductEndpoints {
         .file(jsonProduct)
         .file(imageFile)
         .file(imageFile2)
-        .principal(() -> "umair")
+        .principal(() -> "ken")
     );
 
     String response = createProduct
@@ -124,7 +135,19 @@ public class TestProductEndpoints {
   }
 
   @Test
-  void getAllProductsByCategorySuccess() throws Exception {
+  @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
+      statements = """
+      INSERT INTO account (id, first_name, last_name, date_of_birth, email, password, username) VALUES ('3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', 'Ken', 'Thompson', '1943-02-04', 'ken@example.com', '$2a$10$gIwb60Eio1J1UYWqCrV4je9kAzsqra0kzwg5fcKRCauzGUQ2xmx3q', 'ken');
+      INSERT INTO product_category (id, name) VALUES ('4205756e-6f31-4c34-b8ed-52aca7a64fbf', 'kebab');
+      INSERT INTO product (id, name, product_category, price, condition, is_purchased, description, seller, buyer, color, production_year) VALUES ('3ce17658-9107-4154-9ead-e22c5d6508a5', 'name' ,'4205756e-6f31-4c34-b8ed-52aca7a64fbf', 500, 0, false, 'description', '3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', null, 0, 2024);
+      """)
+  @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
+      statements = """
+      DELETE FROM product WHERE id = '3ce17658-9107-4154-9ead-e22c5d6508a5';
+      DELETE FROM product_category WHERE id = '4205756e-6f31-4c34-b8ed-52aca7a64fbf';
+      DELETE FROM account WHERE id = '3a45dc5e-2a30-41ba-b488-ca4b113ea5ee';
+      """)
+  public void getAllProductsByCategorySuccess() throws Exception {
     ResultActions getProducts = mockMvc.perform(get("/v1/products?category=kebab"));
 
     String response = getProducts
@@ -142,8 +165,20 @@ public class TestProductEndpoints {
   }
 
   @Test
-  void deleteProductSuccess() throws Exception {
-    String responseCreateProduct = Utils.createProduct(mockMvc);
+  @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
+      statements = """
+      INSERT INTO account (id, first_name, last_name, date_of_birth, email, password, username) VALUES ('3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', 'Ken', 'Thompson', '1943-02-04', 'ken@example.com', '$2a$10$gIwb60Eio1J1UYWqCrV4je9kAzsqra0kzwg5fcKRCauzGUQ2xmx3q', 'ken');
+      INSERT INTO product_category (id, name) VALUES ('4205756e-6f31-4c34-b8ed-52aca7a64fbf', 'kebab');
+""")
+  @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
+      statements = """
+      DELETE FROM product_category;
+      DELETE FROM product;
+      DELETE FROM account;
+      """)
+  public void deleteProductSuccess() throws Exception {
+    String responseCreateProduct = Utils.createProduct(mockMvc,
+        "4205756e-6f31-4c34-b8ed-52aca7a64fbf", "ken");
 
     ObjectMapper mapper = new ObjectMapper();
     JsonNode jsonNode = mapper.readTree(responseCreateProduct);
@@ -156,11 +191,23 @@ public class TestProductEndpoints {
     String endPoint = "/v1/products/" + id.substring(1, id.length() - 1);
 
     ResultActions getProducts = mockMvc.perform(delete(endPoint)
-        .principal(() -> "umair"));
+        .principal(() -> "ken"));
     getProducts.andExpect(status().isOk());
   }
 
   @Test
+  @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
+      statements = """
+      INSERT INTO account (id, first_name, last_name, date_of_birth, email, password, username) VALUES ('3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', 'Ken', 'Thompson', '1943-02-04', 'ken@example.com', '$2a$10$gIwb60Eio1J1UYWqCrV4je9kAzsqra0kzwg5fcKRCauzGUQ2xmx3q', 'ken');
+      INSERT INTO product_category (id, name) VALUES ('4205756e-6f31-4c34-b8ed-52aca7a64fbf', 'kebab');
+      INSERT INTO product (id, name, product_category, price, condition, is_purchased, description, seller, buyer, color, production_year) VALUES ('798bdcaf-03c7-4fec-8b87-482ed7cac83d', 'name' ,'4205756e-6f31-4c34-b8ed-52aca7a64fbf', 500, 0, false, 'description', '3a45dc5e-2a30-41ba-b488-ca4b113ea5ee', null, 0, 2024);
+      """)
+  @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
+      statements = """
+      DELETE FROM product WHERE id = '798bdcaf-03c7-4fec-8b87-482ed7cac83d';
+      DELETE FROM product_category WHERE id = '4205756e-6f31-4c34-b8ed-52aca7a64fbf';
+      DELETE FROM account WHERE id = '3a45dc5e-2a30-41ba-b488-ca4b113ea5ee';
+      """)
   public void getProductByIdSuccessful() throws Exception {
     String id = "798bdcaf-03c7-4fec-8b87-482ed7cac83d";
     String endPoint = "/v1/products/" + id;
