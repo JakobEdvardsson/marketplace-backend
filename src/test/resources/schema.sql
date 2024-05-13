536900719f8e04a69fead8b03258e51f2be9dd91
Filip Bengtegård <bengtegardbook@gmail.com>
2024-05-13 16:03:36 +0200

    change is_purchased to status row in product table

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -33,58 +33,56 @@ create index inbox_receiver_id_index
 
 create table product_category
 (
-    id   uuid default gen_random_uuid() not null
-        constraint product_category_pk
-            primary key,
-    name varchar                        not null
-        constraint unique_product_category_name
-            unique
+  id   uuid default gen_random_uuid() not null
+    constraint product_category_pk
+      primary key,
+  name varchar                        not null
+    constraint unique_product_type_name
+      unique
 );
 
 create table product
 (
-
-    id               uuid                     default gen_random_uuid() not null
-        constraint product_pk
-            primary key,
-    name             varchar                                            not null,
-    product_category uuid                                               not null
-        constraint product_category_id_fk
-            references product_category,
-    price            integer                                            not null,
-    condition        integer                                            not null
-        constraint check_condition_range
-            check ((condition >= 0) AND (condition <= 4)),
-    is_purchased     boolean                                            not null,
-    description      text                                               not null,
-    seller           uuid                                               not null
-        constraint product_seller_fk
-            references account,
-    buyer            uuid
-        constraint product_buyer_fk
-            references account,
-    color            integer,
-    production_year  integer
-        constraint check_year
-            check ((production_year >= 2000) AND (production_year <= 2100)),
-    created_at       timestamp with time zone default now()             not null,
-    constraint check_valid_purchase_status
-        check (((buyer IS NULL) AND (is_purchased IS FALSE)) OR
-               ((buyer IS NOT NULL) AND (is_purchased IS TRUE)))
-
+  id               uuid                     default gen_random_uuid() not null
+    constraint product_pk
+      primary key,
+  name             varchar                                            not null,
+  product_category uuid                                               not null
+    constraint product_type_id_fk
+      references product_category,
+  price            integer                                            not null,
+  condition        integer                                            not null
+    constraint check_condition_range
+      check ((condition >= 0) AND (condition <= 4)),
+  status           integer                                            not null,
+  description      text                                               not null,
+  seller           uuid                                               not null
+    constraint product_seller_fk
+      references account,
+  buyer            uuid
+    constraint product_buyer_fk
+      references account,
+  color            integer,
+  production_year  integer
+    constraint check_year
+      check ((production_year >= 2000) AND (production_year <= 2100)),
+  created_at       timestamp with time zone default now()             not null,
+  constraint check_valid_purchase_status
+    check (((buyer IS NULL) AND (status = 0)) OR ((buyer IS NOT NULL) AND (status = 1)) OR
+           ((buyer IS NOT NULL) AND (status = 2)))
 );
 
 create index product_buyer_index
   on product (buyer);
-
-create index product_is_purchased_index
-  on product (is_purchased);
 
 create index product_seller_index
   on product (seller);
 
 create index product_product_category_index
   on product (product_category);
+
+create index product_is_purchased_index
+  on product (status);
 
 create table product_image
 (
@@ -132,21 +130,19 @@ create index product_order_buyer_id_index
 
 create table watchlist
 (
-
-    product_category_id uuid                           not null
-        constraint watchlist_product_category_id_fk
-            references product_category,
-    subscriber_id   uuid                           not null
-        constraint watchlist_account_id_fk
-            references account,
-    id              uuid default gen_random_uuid() not null
-        constraint watchlist_pk
-            primary key
+  product_category_id uuid                           not null
+    constraint watchlist_product_type_id_fk
+      references product_category,
+  subscriber_id       uuid                           not null
+    constraint watchlist_account_id_fk
+      references account,
+  id                  uuid default gen_random_uuid() not null
+    constraint watchlist_pk
+      primary key
 );
 
 create index watchlist_product_category_id_index
-    on watchlist (product_category_id);
-
+  on watchlist (product_category_id);
 
 create index watchlist_subscriber_id_index
   on watchlist (subscriber_id);
